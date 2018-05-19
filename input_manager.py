96a2ceb46fac1ded7f3d7caafc54fc7d7961f7c0
Joe MacInnes <joemacinnes@Joes-MacBook-Air.local>
2018-05-19 14:04:52 -0400

    Add text functionality

--- input_manager.py
+++ input_manager.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import threading
 ser = serial.Serial('/dev/cu.usbmodem1421',9600)
 
 #Potentiometer range
@@ -14,7 +15,9 @@ class InputManager():
     def read_raw_inputs(self):
         # Read in all the info waiting in the serial connection
         serial_content = ser.read(ser.inWaiting())
+
         try:
+            serial_content = serial_content.decode()
             split_content = serial_content.split('\n')
             # Get the second to last set of values sent from Arduino
             raw_info = split_content[-2].rstrip('/\r')
@@ -29,32 +32,54 @@ class InputManager():
             print(e)
             return None
 
-    def add_input(self, pin_number, features, reading_range):
-        self.inputs[pin_number] = Input(features, reading_range)
+    def add_input(self, pin_number, features, label, reading_range):
+        self.inputs[pin_number] = Input(features, label, reading_range)
 
     def get_inputs(self):
-        inputs = []
+        inputs = {}
         raw = self.read_raw_inputs()
         try:
             for key in raw:
                 reading = raw[key]
-                inputs.append(self.inputs[key].select_feature(reading))
+                param, value = self.inputs[key].select_feature(reading)
+                inputs[param] = value
             return inputs
         except Exception as e:
             print(e)
             return None
 
 class Input():
-    def __init__(self, features, reading_range):
+    def __init__(self, features, label, reading_range):
         self.features = features
+        self.label = label
         self.reading_range = reading_range
         self.features_len = len(features)
 
     def select_feature(self, reading):
         # I need to map this to smaller values and choose a feature
         index = int(float(reading) * self.features_len / self.reading_range)
-        return self.features[index]
+        return self.label, self.features[index]
 
+
+class InputThread (threading.Thread):
+   def __init__(self, threadID, name, counter, inputs):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.inputs = inputs
+      self.counter = counter
+      self.input_readings = {}
+   def run(self):
+      print ("Starting " + self.name)
+      self.continuous_read()
+      print ("Exiting " + self.name)
+   def continuous_read(self):
+      while True:
+          time.sleep(.1)
+          #print (self.inputs.get_inputs())
+          self.input_readings = self.inputs.get_inputs()
+   def get_readings(self):
+       return self.input_readings
 
 
 
